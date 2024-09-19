4ae43bcfa5b925e8a86d6d630fba0d09ef25764d
Khen Channmakara <khenchannmakara@gmail.com>
2024-09-20 01:13:07 +0700

    chang

--- src/app/api/orders/route.js
+++ src/app/api/orders/route.js
@@ -1,8 +1,6 @@
 import { NextResponse } from "next/server";
-
 export async function POST(request) {
   const { foodName, name, address, quantity } = await request.json();
-
   const order = {
     id: new Date().getTime(),
     foodName,
@@ -11,9 +9,7 @@ export async function POST(request) {
     quantity,
     createdAt: new Date().toISOString(),
   };
-
   console.log("Order received:", order);
-
   return NextResponse.json(
     { message: "Order placed successfully!", order },
     { status: 201 }
